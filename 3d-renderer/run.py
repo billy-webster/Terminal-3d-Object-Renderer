70150cded15c545b958c8b64f36ae76269ad466e
Billy Webster <95182637+billy-webster@users.noreply.github.com>
2025-11-12 13:50:13 +0000

    Delete 3d-renderer directory

--- 3d-renderer/run.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from src.main import main
-import curses
-
-if __name__ == "__main__":
-    curses.wrapper(main)
--- 3d-renderer/src/shapes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .cube import Cube
-from .pyramid import Pyramid
-from .house import HouseShape
-from .cone import ConeShape
